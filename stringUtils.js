ab6ad8b24eb26aabe9edc65227191e3ada5a412b
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-05-19 14:29:18 -0500

    upload hw

---
+++ stringUtils.js
@@ -0,0 +1,14 @@
+// stringUtils.js
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + String.slice(1)
+}
+
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+function contains(str, substr) {
+
+  
+}
